c0c0e47bf566862b0c5b054767e787404f5de0f9
stephenwayar <stephenbuluswayar@gmail.com>
2024-03-04 09:46:05 +0100

    fix: minor changes

--- routes/user.ts
+++ routes/user.ts
@@ -3,6 +3,6 @@ import { get_user_profile } from '../controllers/UserController';
 
 const Router = express.Router();
 
-Router.post('/api/user/:id', get_user_profile);
+Router.get('/api/user/:id', get_user_profile);
 
 export default Router;
